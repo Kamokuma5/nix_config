b43b8e43288e6db9bc093eaa14cde7e851ae8ee9
Kamo Kuma <kamokuma5@gmail.com>
2025-01-23 00:05:18 -0500

    latest nix packages has more up to date Nvidia driver

--- nixos_config/nixos_modules/nvidia.nix
+++ nixos_config/nixos_modules/nvidia.nix
@@ -16,14 +16,7 @@
     open = false;
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "565.57.01";
-      sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
-      sha256_aarch64 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
-      openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
-      settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
-      persistencedSha256 = lib.fakeHash;
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
 
     prime = {
       offload = {
