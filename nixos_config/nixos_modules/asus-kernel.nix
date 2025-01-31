f9691c32c664b7be7c8f5383a13d234d30c69d2d
Kamo Kuma <kamokuma5@gmail.com>
2025-01-31 16:13:36 -0500

    Update kernel to 6.13 and Nvidia drivers to 570.86.16

--- nixos_config/nixos_modules/asus-kernel.nix
+++ nixos_config/nixos_modules/asus-kernel.nix
@@ -15,24 +15,24 @@
         buildLinux (
           args
           // rec {
-            version = "6.12.10-arch1";
-            modDirVersion = version;
+            version = "6.13-arch1";
+            modDirVersion = "6.13.0-arch1";
 
             patch_dir = fetchgit {
               url = "https://aur.archlinux.org/linux-g14.git";
-              rev = "17da925db82998b79144053b7a518656b0d51f4a";
-              hash = "sha256-uSZzsetlXfJXO38dgIS29VVBuBexfKh7vfI862r95GA=";
+              rev = "485b2b1f5abc703faed64f4fdaa741fe00e5cf19";
+              hash = "sha256-HnT4vRi1l2W9UpqS7uC1mxJyyah00y60RHtwHPat42k=";
             };
 
             src = fetchzip {
               url = "https://github.com/archlinux/linux/archive/refs/tags/v${version}.tar.gz";
-              hash = "sha256-2rP0I2dN4AOjNaOgMeDzZJw881EYYVUMdagR8D2ds+k=";
+              hash = "sha256-kZBqtH5Cwu6exVouU9XkiO/fiT3uKVrrrF1tQyAb1jg=";
             };
             kernelPatches =
               with {
                 patch_series = fetchurl {
-                  url = "https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.12/asus-patch-series.patch";
-                  hash = "sha256-ik68C107YsjmZPZGu2AL2fWchaKitl5DuxhcuJnvQgo=";
+                  url = "https://gitlab.com/asus-linux/fedora-kernel/-/raw/rog-6.13/asus-patch-series.patch";
+                  hash = "sha256-ECOUhk1TF8VHuOp+LZQTW35VKw6m66SdS9m8FtGgNkA=";
                 };
               };
 
@@ -45,10 +45,10 @@
                   name = "0001-acpi-proc-idle-skip-dummy-wait.patch";
                   patch = "${patch_dir}/0001-acpi-proc-idle-skip-dummy-wait.patch";
                 }
-                {
-                  name = "0002-mt76_-mt7921_-Disable-powersave-features-by-default.patch";
-                  patch = "${patch_dir}/0002-mt76_-mt7921_-Disable-powersave-features-by-default.patch";
-                }
+                # {
+                #   name = "0002-mt76_-mt7921_-Disable-powersave-features-by-default.patch";
+                #   patch = "${patch_dir}/0002-mt76_-mt7921_-Disable-powersave-features-by-default.patch";
+                # }
                 {
                   name = "0004-ACPI-resource-Skip-IRQ-override-on-ASUS-TUF-Gaming-A.patch";
                   patch = "${patch_dir}/0004-ACPI-resource-Skip-IRQ-override-on-ASUS-TUF-Gaming-A.patch";
@@ -57,10 +57,10 @@
                   name = "0005-ACPI-resource-Skip-IRQ-override-on-ASUS-TUF-Gaming-A.patch";
                   patch = "${patch_dir}/0005-ACPI-resource-Skip-IRQ-override-on-ASUS-TUF-Gaming-A.patch";
                 }
-                {
-                  name = "0006-mediatek-pci-reset.patch";
-                  patch = "${patch_dir}/0006-mediatek-pci-reset.patch";
-                }
+                # {
+                #   name = "0006-mediatek-pci-reset.patch";
+                #   patch = "${patch_dir}/0006-mediatek-pci-reset.patch";
+                # }
                 {
                   name = "0007-workaround_hardware_decoding_amdgpu.patch";
                   patch = "${patch_dir}/0007-workaround_hardware_decoding_amdgpu.patch";
@@ -76,6 +76,10 @@
                 {
                   name = "0010-asus-nb-wmi-fix-tablet_mode_sw_int.patch";
                   patch = "${patch_dir}/0010-asus-nb-wmi-fix-tablet_mode_sw_int.patch";
+                }
+                {
+                  name = "0011-amdgpu-adjust_plane_init_off_by_one.patch";
+                  patch = "${patch_dir}/0011-amdgpu-adjust_plane_init_off_by_one.patch";
                 }
               ];
 
